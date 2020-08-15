11cc2210158217de27933964bf00acbf24b58c19
Son Luu <sonlh.cs.uit@gmail.com>
2020-08-15 18:08:01 +0700

    asynchrous

--- test.js
+++ test.js
@@ -1,21 +1,14 @@
-function square(x) {
-    if (Number.isNaN(Number(x))) {
-        throw new Error("x must be a number")
-    } else {
-        return x ** 2
-    }
-}
-for (let i = 0; i < 10; i++) {
-    try {
-        let sq;
-        if (i == 3) {
-            sq = square("hehe")
-        } else {
-            sq = square(i)
-        }
-        console.log(sq)
-    } catch (e) {
-        console.error(e.name + ': ' + e.message)
-    }
 
+async function getgg(){
+    return await fetch("http://google.com",{
+        mode:"no-cors"
+    }).then(res=>{
+        return res
+    })
 }
+console.log(getgg().then(res=>{
+    console.log(res)
+}))
+
+
+
