6cbda45149204d350043f074a5293c9c7fa276ee
Son Luu <sonlh.cs.uit@gmail.com>
2020-12-06 15:21:13 +0700

    add card & filter game

---
+++ cardMemoryGame/pokemon.js
@@ -0,0 +1,12 @@
+const back = 'https://upload.wikimedia.org/wikipedia/vi/3/3b/Pokemon_Trading_Card_Game_cardback.jpg'
+const finish = 'https://media.istockphoto.com/illustrations/cartoon-finish-sign-illustration-id478290143'
+let pokemon_data = [
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png',
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png',
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/005.png',
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png',
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png',
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/008.png',
+]
---
+++ filterPokemonApp/pokemon.js
@@ -0,0 +1,48 @@
+export let pokemon = [
+    {
+      avatar: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/114.png',
+      type: ['grass']
+    }, {
+      avatar: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/008.png',
+      type: ['water']
+    }, {
+      avatar: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/009.png',
+      type: ['water']
+    }, {
+      avatar: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/038.png',
+      type: ['fire']
+    }, {
+      avatar: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/152.png',
+      type: ['grass']
+    }, {
+      avatar: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/155.png',
+      type: ['fire']
+    }, {
+      avatar: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/158.png',
+      type: ['water']
+    }, {
+      avatar: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/182.png',
+      type: ['grass']
+    }, {
+      avatar: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/186.png',
+      type: ['water']
+    }, {
+      avatar: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/240.png',
+      type: ['fire']
+    }, {
+      avatar: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/254.png',
+      type: ['grass']
+    }, {
+      avatar: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/258.png',
+      type: ['water']
+    }, {
+      avatar: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/331.png',
+      type: ['grass']
+    }, {
+      avatar: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/285.png',
+      type: ['grass']
+    },{
+      avatar: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/341.png',
+      type: ['water']
+    }
+  ]
