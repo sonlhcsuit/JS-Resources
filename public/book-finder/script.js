e80aadc774a15c39110bd6664a881693f9f2aed7
sonlhcsuit <sonlh.cs.uit@gmail.com>
2021-05-15 21:45:16 +0700

    update book-finder

--- public/book-finder/script.js
+++ public/book-finder/script.js
@@ -2,7 +2,7 @@ function render({volumeInfo,previewLink}){
     let {authors} = volumeInfo
     authors = authors || 'N/A'
     return `<div class="card col-md-3 m-md-0 h-md-0" >
-                <img class="card-img-top" src="${volumeInfo.imageLinks.smallThumbnail}" alt="Card image cap">
+                <img class="card-img-top" src="${volumeInfo.imageLinks?.smallThumbnail }" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title">${volumeInfo.title}</h5>
                     <p class="card-text">${authors == 'N/A'?authors:authors.join(', ')}</p>
